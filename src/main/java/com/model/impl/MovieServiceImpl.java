d0c1a132325263bb67fcd0b23bbf2fab00af001c
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-12-12 10:17:15 +0200

    added simple logging

--- src/main/java/com/model/impl/MovieServiceImpl.java
+++ src/main/java/com/model/impl/MovieServiceImpl.java
@@ -1,16 +1,18 @@
 package com.model.impl;
 
-import com.repository.MovieRepository;
 import com.dto.MovieDto;
 import com.model.MovieService;
 import com.model.mapper.MovieMapper;
+import com.repository.MovieRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 public class MovieServiceImpl implements MovieService {
     private final MovieRepository movieDao;
@@ -18,12 +20,14 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public List<MovieDto> getAllMovies() {
+        log.info("Get all movies");
         return movieDao.getAllMovies().stream()
                 .map(movieMapper::mapToDto)
                 .collect(Collectors.toList());
     }
 
     public List<MovieDto> getRandomMovies(int count) {
+        log.info("Ger {} random movie", count);
         return movieDao.getRandomMovies(count).stream()
                 .map(movieMapper::mapToDto)
                 .collect(Collectors.toList());
