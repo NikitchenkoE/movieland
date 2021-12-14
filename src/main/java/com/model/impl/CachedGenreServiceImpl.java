ffe2c763a27f1df00032e6efc17198b4ad8398a2
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-12-14 14:08:37 +0200

    t-1 created CachedGenreServiceImpl.java

---
+++ src/main/java/com/model/impl/CachedGenreServiceImpl.java
@@ -0,0 +1,33 @@
+package com.model.impl;
+
+import com.dto.GenreDto;
+import com.model.GenreService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CachedGenreServiceImpl implements GenreService{
+    private final GenreServiceImpl genreService;
+    private List<GenreDto> genreCache = new ArrayList<>();
+
+    public List<GenreDto> getAllGenreDto(){
+        log.info("get cached list of GenreDto");
+        if (genreCache.isEmpty()) {
+            genreCache = genreService.getAllGenreDto();
+        }
+        return genreCache;
+    }
+
+    @Scheduled(fixedDelay = 4 * 60 * 60 * 1000)
+    public void clearGenreCache() {
+        genreCache.clear();
+        log.info("genre cache cleared");
+    }
+}
--- src/main/java/com/web/controller/GenresController.java
+++ src/main/java/com/web/controller/GenresController.java
@@ -2,16 +2,19 @@ package com.web.controller;
 
 import com.dto.GenreDto;
 import com.model.GenreService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
 public class GenresController {
     private final GenreService genreService;
+
+    public GenresController(@Qualifier("cachedGenreServiceImpl") GenreService genreService) {
+        this.genreService = genreService;
+    }
 
     @GetMapping("/genre")
     public List<GenreDto> getAllGenresDto() {
