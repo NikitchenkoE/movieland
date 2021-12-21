0ebc9ed473dcbb45a95fedf0f8fb5a2d5b6b306f
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-12-21 10:41:15 +0200

    s-1 deleted appendix annotation

--- src/main/java/com/MovieLandApplication.java
+++ src/main/java/com/MovieLandApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.config")
 public class MovieLandApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieLandApplication.class, args);
