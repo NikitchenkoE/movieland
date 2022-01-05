a1141bd0153c67a3b0b6e2704b1b9e4007769cbc
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2022-01-05 15:00:13 +0200

    b-10 found and fixed problem in security in review

--- src/main/java/com/web/controller/ReviewController.java
+++ src/main/java/com/web/controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.web.controller;
 import com.dto.ReviewDtoWithoutUser;
 import com.model.ReviewService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.security.Principal;
 public class ReviewController {
     private final ReviewService reviewService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/review")
     public void addReview(@RequestBody ReviewDtoWithoutUser review,
                           Principal principal) {
